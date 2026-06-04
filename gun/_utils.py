41968c1751a735b1885c1dd2df8e8b17d49fc45c
Richard Iannone <riannone@me.com>
2026-06-03 23:13:15 -0400

    Add find_open_port utility

--- gun/_utils.py
+++ gun/_utils.py
@@ -16,3 +16,10 @@ def file_url(path: str | Path) -> str:
     return "file://" + quote(str(p), safe="/:")
 
 
+def find_open_port() -> int:
+    """Find an available TCP port on localhost."""
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("127.0.0.1", 0))
+        return s.getsockname()[1]
+
+
